ca4b4152f7f1b395f356f9851e02e5d873409e11
Maria-Sousa-GH <157738574+Maria-Sousa-GH@users.noreply.github.com>
2024-03-13 14:48:51 +0000

    created unction searchCity(city)

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-
+function searchCity(city) {
+  apikey = "facba5oe04e0d0e2497c5fe825bta3c4";
+  apiURL =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+}
