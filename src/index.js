b725a50cda7b213f70596c8b7eeec481c8404e6f
Maria-Sousa-GH <157738574+Maria-Sousa-GH@users.noreply.github.com>
2024-03-13 14:42:01 +0000

    added function handleSearchSubmit

--- src/index.js
+++ src/index.js
@@ -1,59 +1,11 @@
-function search(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
-  SearchCity(searchInput.value);
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-function SearchCity(city) {
-  let apiKey = "facba5oe04e0d0e2497c5fe825bta3c4";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
-  axios.get(apiUrl).then(refreshWeather);
-}
 
-function refreshWeather(response) {}
-
-function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let day = date.getDay();
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let formattedDay = days[day];
-  return `${formattedDay} ${hours}:${minutes}`;
-}
-
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
-
-currentDateELement.innerHTML = formatDate(currentDate);
-
-function signUp(event) {
-  event.preventDefault();
-  let input = document.querySelector("#email");
-  console.log(input.value);
-}
-let form = document.querySelector("form");
-form.addEventListener("submit", signUp);
