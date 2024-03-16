4d08707b2d93ef65454ae7f7aa7f047bc7ca4af1
Maria-Sousa-GH <157738574+Maria-Sousa-GH@users.noreply.github.com>
2024-03-16 10:53:56 +0000

    adjusted js, html and css for the function displayForecast

--- src/index.js
+++ src/index.js
@@ -52,7 +52,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">☀</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>18º</strong>
+      </div>
+      <div class="weather-forecast-temperature"> 12º </div>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Lisbon");
+displayForecast();
